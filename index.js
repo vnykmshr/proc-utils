b4d23f45770c1a68e6c7f6b1a9a39381fe8b477e
Vinayak Mishra <viks@vnykmshr.com>
2014-09-16 11:55:26 +0545

    Adds extend to clone javascript objects

--- index.js
+++ index.js
@@ -3,5 +3,6 @@
 var proc = require('./lib/proc');
 proc.utils = require('./lib/utils');
 proc.predicate = require('./lib/predicate');
+proc.extend = require('./lib/extend');
 
 module.exports = proc;
