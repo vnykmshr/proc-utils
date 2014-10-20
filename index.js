fd2f4b31d3aa5dba5853df7be082555763799e5d
Vinayak Mishra <viks@vnykmshr.com>
2014-10-20 23:28:59 +0545

    Adds scan routine to scan directories for files

--- index.js
+++ index.js
@@ -4,5 +4,13 @@ var proc = require('./lib/proc');
 proc.utils = require('./lib/utils');
 proc.predicate = require('./lib/predicate');
 proc.extend = require('./lib/extend');
+proc.scan = require('./lib/scan');
 
 module.exports = proc;
+
+//-- Test Code ----------------------------------------------------------
+if (require.main === module) {
+    (function () {
+        console.log(proc);
+    })();
+}
