9bf6727a4b2feff1d38dc380d789fe94e304a645
Vinayak Mishra <viks@vnykmshr.com>
2014-10-21 10:28:43 +0545

    Adds make api call to proc utils

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ proc.utils = require('./lib/utils');
 proc.predicate = require('./lib/predicate');
 proc.extend = require('./lib/extend');
 proc.scan = require('./lib/scan');
+proc.makeApiCall = require('./lib/make-api-call');
 
 module.exports = proc;
 
